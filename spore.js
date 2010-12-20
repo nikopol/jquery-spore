a938f9a0521b43ba551dac88bc1fbceb131c8b49
niko <nikomomo@gmail.com>
2010-12-20 12:48:26 +0100

    core functional, ihackernews sample added, widgets to come

--- spore.js
+++ spore.js
@@ -3,47 +3,46 @@
 
 var spore = {
 	
-	log: function(){};
+	log: function(){},
 		
-	debug: function(state){
+	debug: function(state) {
 		if(state) {
+			var pfx='[spore] ';
 			if(window.console && window.console.debug) //firebug
-				spore.log=window.console.debug;
+				spore.log=function(m){ window.console.debug(pfx+m); };
 			else if(window.console && window.console.log) //safari
-				spore.log=window.console.log;
+				spore.log=function(m){ window.console.log(pfx+m); };
 			else if(console && console.log) //chrome
-				spore.log=console.log;
+				spore.log=function(m){ console.log(pfx+m); };
 			else if(opera && opera.postError) //opera
-				spore.log=opera.postError;
-			else
-				spore.log=alert;
+				spore.log=function(m){ opera.postError(pfx+m); };
 		} else
 			spore.log=function(){};
 	},
 
-	create: function(spec,over) {
+	create: function(spec,callback,over) {
 		if(typeof(spec)=='string') {
-			if(/^http:\/\//i.test(spec)) {
-				//todo: spec cache
-				if(!spec.callback) throw "your must provide a callback with a spec over http";
-				$.get(spec,function(data){ 
-					spec.callback( spore.create(data,over) ); 
-				});
-				return true;
-			} else
+			if(/^\s*{/.test(spec)) 
 				spec=$.parseJSON(spec);
+			else {
+				//todo: spec cache
+				if(!callback) throw "your must provide a callback with a spec over http";
+				$.get(spec,function(data){ spore.create(data,callback,over) }); 
+				return true;
+			}
 		} else if(typeof(spec)!='object')
 			throw "wrong spore creation parameters";
 
 		if(over) spec=$.extend(spec,over);
-		return new spore.api(spec);
+		var a=new spore.api(spec);
+		if(callback) callback(a);
+		return a;
 	},
 
-	new_from_string: function(spec,over) { return spore.create(spec,over); },
-	new_from_spec: function(spec,over) { return spore.create(spec,over); },
-
 	api: function(spec,widgets) {
-		if(!spec.api_base_url) throw 'parameter api_base_url missing';
+		if(!spec.base_url) throw 'parameter base_url missing';
+		if(!/https?\:\/\//i.test(spec.base_url)) spec.base_url='http://'+spec.base_url;
+		spec.base_url.replace(/\/$/,'');
 		this.spec=$.extend({
 			api_format: 'json',
 			authentication: false,
@@ -55,15 +54,17 @@ var spore = {
 		spore.log('spore init api '+this.spec.name+' v'+this.spec.version);
 	
 		//create methods
-		for(var fn as this.spec.methods) {
-			var m=this.spec.methods[fn];
-			if(m.path[0]=='/') m.path=m.path.substr(1);
-			m.method||='GET';
-			m.params||=[];
-			this[fn]=function(params,onsuccess,onerror) {
-				return this._call(fn,params,onsuccess,onerror);
+		var self=this;
+		var wrapfn=function(fn){
+			return function(params,onsuccess,onerror){
+				self._call(fn,params,onsuccess,onerror);
 			};
-			console.log('spore: '+this.spec.name+'.'+fn+' created');
+		};
+		for(var fn in this.spec.methods) {
+			if(!/^\//.test(this.spec.methods[fn].path))
+				this.spec.methods[fn].path='/'+this.spec.methods[fn].path;
+			this[fn]=wrapfn(fn);
+			spore.log(this.spec.name+'.'+fn+' created');
 		}
 	},
 	
@@ -71,60 +72,68 @@ var spore = {
 };
 
 spore.api.prototype._call=function(fn,params,onsuccess,onerror) {
-	var method=this.spec.methods[fn]
-	var url=this.spec.api_base_url+method.path;
-	//manage path params
-	for(var n=0; n<method.required.length; ++n) {
-		var p=method.required[n];
-		if(typeof(params[p])=='undefined') throw 'param '+p+' missing';
-		url=url
-			.replace('/\:'+p+'\//g', params[p]+'/')
-			.replace('/\:'+p+'$/g', params[p]);
-	}
-	//manage url params
-	var args={};
-	for(var n=0; n<method.params.length; ++n) {
-		var p=method.params[n];
-		if(typeof(params[p])!='undefined') args[p]=params[p];
-	}
-	
-	
-	;
+	var method=this.spec.methods[fn],
+		url=this.spec.base_url+method.path,
+		prm=$.extend({},params),
+		plog=this.spec.name+'.'+fn+' ';
+		dta={};
+	spore.log(plog+'init('+(params==undefined?'':(JSON?JSON.stringify(params):'...'))+')');
+	//manage params
+	if(method.required_params)
+		for(var n=0; n<method.required_params.length; ++n) {
+			var p=method.required_params[n];
+			if(prm[p]==undefined) throw 'param '+p+' required';
+			var r=new RegExp("(\:"+p+")(\/|$)");
+			if(r.test(url)) url=url.replace(r,prm[p]+'$2');
+			else dta[p]=prm[p];
+		}
+	if(method.optional_params)
+		for(var n=0; n<method.optional_params.length; ++n) {
+			var p=method.optional_params[n],
+				d=(prm[p]!=undefined),
+				r=new RegExp("(\:"+p+")(\/|$)");
+			if(r.test(url)) url=url.replace(r,d?prm[p]+'$2':'$2');
+			else if(d) dta[p]=prm[p];
+		}
 	//call widgets
 	for(var n=0; n<this.widgets.length; n++) {
-		var w=this.widgets[n];
+		var w=this.widgets[n], wlog=plog+'widget '+w.name+' ';
 		try {
-			var o=w.call(req);
+			var o=w.call(req,dta);
 			if(o===false) {
-				spore.log('spore.'+w.name+' returned false, call stopped!');
+				spore.log(wlog+'returned false, call stopped!');
 				return false;
 			} else if(typeof(o)=='function') {
-				spore.log('spore.'+w.name+' returned a callback');
+				spore.log(wlog+'returned a callback');
 				w.callback=o;
 			} else {
-				spore.log('spore.'+w.name+' returned '+o);
+				spore.log(wlog+'returned '+o);
 			}
 		} catch(e) {
-			spore.log('spore.'+w.name+' error! => '+e);
+			spore.log(wlog+'error! => '+e);
 		}
 	}
 	//webservice's call
-	spore.log('spore.'+method.name+'] call '+url);
+	spore.log(plog+'call '+url);
+	var self=this;
+	dta['format']='jsonp';
 	$.ajax({
 		url: url,
 		type: method.method,
-		data: args,
-		contentType: this.api_format,
+		data: dta,
+		dataType: 'jsonp',
+		contentType: 'json', //this.spec.formats[0]
 		success: function(data,status,req) {
-			console.log('spore: '+this.spec.name+':'+method.name+'] return '+status);
-			for(var n=this.widgets.length-1; n>=0; --n) {
-				var w=this.widgets[n];
+			spore.log(plog+'return '+(data?"datas":"nothing"));
+			//call widgets backwards
+			for(var n=self.widgets.length-1; n>=0; --n) {
+				var w=self.widgets[n], wlog=plog+'widget '+w.name+' ';
 				if(w.callback) {
 					try {
 						var r=w.callback(data,req);
-						spore.log('spore.'+w.name+' callback returned '+r);	
+						spore.log(wlog+'callback returned '+r);	
 					} catch (e) {
-						spore.log('spore.'+w.name+' callback error! => '+e);
+						spore.log(wlog+'callback error! => '+e);
 					}
 					delete w.callback;
 				}
@@ -132,8 +141,8 @@ spore.api.prototype._call=function(fn,params,onsuccess,onerror) {
 			if(onsuccess) onsuccess(data);
 		},
 		error: function(req,status,err) {
-			console.log('spore: '+this.spec.name+':'+method.name+'] return '+status);
-			if(onerror) onerror(err,status,req);
+			spore.log(plog+'server returned '+req.status+':'+req.statusText);
+			if(onerror) onerror(req);
 		}
 	});
 	return true;
